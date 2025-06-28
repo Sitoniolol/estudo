276464297e7a64efc272a51ec807b3106e1fac09
Sitoniolol <rylersenna@gmail.com>
2025-06-28 11:50:20 -0300

    Commit Inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/usuarios/:APARECE', function (request, response) {
+
+    console.log(request.params)
+
+    response.send('Ola deu bom')
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Meu servidor ta ok")
+})
